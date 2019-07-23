faa89597abff7c9fd32c52e4d62c8130033759e5
dai <msdenisseanaya@gmail.com>
2019-07-22 17:33:37 -0700

    onActivityResult- lesson "Get the selected image back"

--- app/src/main/java/com/example/learningkotlin/CreateHabitActivity.kt
+++ app/src/main/java/com/example/learningkotlin/CreateHabitActivity.kt
@@ -1,14 +1,21 @@
 package com.example.learningkotlin
 
+import android.app.Activity
 import android.content.Intent
+import android.graphics.Bitmap
+import android.net.Uri
 import android.nfc.Tag
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore
 import android.util.Log
 import android.view.View
+import kotlinx.android.synthetic.main.activity_create_habit.*
+import java.io.IOException
 
 class CreateHabitActivity : AppCompatActivity() {
     private val TAG = CreateHabitActivity::class.java.simpleName
+    private val CHOOSE_IMAGE_REQUEST = 1
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,8 +27,32 @@ class CreateHabitActivity : AppCompatActivity() {
         intent.action = Intent.ACTION_GET_CONTENT
 
         val chooser = Intent.createChooser(intent, "Choose image for habit")
-        startActivity(chooser)
+        startActivityForResult(chooser,CHOOSE_IMAGE_REQUEST)
 
         Log.d(TAG, "Intent to choose image sent...")
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == CHOOSE_IMAGE_REQUEST && resultCode == Activity.RESULT_OK
+            && data != null && data.data != null){
+            Log.d(TAG, "An image was chosen by the user")
+
+            val bitmap = tryReadBitmap(data.data)
+
+            bitmap?.let {
+                iv_image.setImageBitmap(bitmap)
+                Log.d(TAG, "Read image bitmap  and update image view.")
+            }
+        }
+    }
+
+    private fun tryReadBitmap(data: Uri): Bitmap? {
+        return try{
+            MediaStore.Images.Media.getBitmap(contentResolver, data)
+        }catch (e: IOException){
+            e.printStackTrace()
+            null
+        }
     }
 }
