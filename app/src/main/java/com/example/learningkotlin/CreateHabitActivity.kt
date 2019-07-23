b0e5c001ed7a0ec0a20de0ceeb0a8f04a507f508
dai <msdenisseanaya@gmail.com>
2019-07-22 17:19:19 -0700

    onclick active- lesson "Let users choose an image"

--- app/src/main/java/com/example/learningkotlin/CreateHabitActivity.kt
+++ app/src/main/java/com/example/learningkotlin/CreateHabitActivity.kt
@@ -1,12 +1,27 @@
 package com.example.learningkotlin
 
+import android.content.Intent
+import android.nfc.Tag
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
 
 class CreateHabitActivity : AppCompatActivity() {
+    private val TAG = CreateHabitActivity::class.java.simpleName
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_create_habit)
+    }
+    fun chooseImage(v: View){
+        val intent= Intent()
+        intent.type = "image/*"
+        intent.action = Intent.ACTION_GET_CONTENT
+
+        val chooser = Intent.createChooser(intent, "Choose image for habit")
+        startActivity(chooser)
+
+        Log.d(TAG, "Intent to choose image sent...")
     }
 }
